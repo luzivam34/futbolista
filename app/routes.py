343f74d2e5b46e744dec1517523adf76e51b5cd0
Luzy <luzivam.santos24@gmail.com>
2025-04-28 00:53:48 -0300

    Modificando routes.py com a implementação de rotas para excluir e editar criado a file editar.html e implementado para fazer edição modificado index.html com a implementação dos botões

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 main = Blueprint('main', __name__)
 
-@main.route('/')
+@main.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         novo_jogo = Jogo(
@@ -23,3 +23,27 @@ def index():
 
     jogos = Jogo.query.order_by(Jogo.data_jogo.desc()).all()
     return render_template('index.html', jogos=jogos)
+
+@main.route('/editar/<int:id>', methods=['GET', 'POST'])
+def editar(id):
+    jogo = Jogo.query.get_or_404(id)
+    if request.method == 'POST':
+        jogo.data_jogo = datetime.strptime(request.form['data_jogo'], '%Y-%m-%d')
+        jogo.clube_mandante = request.form['mandante']
+        jogo.gols_mandante = int(request.form['gols_mandante'])
+        jogo.gols_visitante = int(request.form['gols_visitante'])
+        jogo.clube_visitante = request.form['visitante']
+        jogo.campeonato = request.form['campeonato']
+        jogo.local_partida = request.form['local']
+
+        db.session.commit()
+        return redirect(url_for('main.index'))
+
+    return render_template('editar.html', jogo=jogo)
+
+@main.route('/excluir/<int:id>', methods=['POST'])
+def excluir(id):
+    jogo = Jogo.query.get_or_404(id)
+    db.session.delete(jogo)
+    db.session.commit()
+    return redirect(url_for('main.index'))
